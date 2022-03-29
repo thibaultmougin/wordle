8d342ab01f17801559a56de83ec90eb998e59d33
Thibault Mougin <thibault.mougin01@gmail.com>
2022-03-29 15:24:57 +0200

    changements

--- wordle.c
+++ wordle.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int len(char* s){
+    int n=0;
+    while (!(s[n]==0)){
+        n++;
+    }
+    return n;
+}
+
 int main(int argc,char const *argv[]){
-
-    printf("sheeeesh");
-
+    char s[20];
+    printf("Ecrivez votre mot de 5 lettres: ");
+    scanf("%19s", s);
+    printf("%d",len(s));
     return 0;
 }
